b7a87949298b9cf818831443887512f4bf70cafe
Jason Walzak <walzak@gmail.com>
2017-06-24 17:18:27 -0400

    Added scores to the sheet changed background colour to dark-blue changed results font colour to off-white changed h2 colour to off-white

--- scripts/main.js
+++ scripts/main.js
@@ -4,33 +4,33 @@ var baseballObject;
 // AJAX XMLHttpRequest to get the JSON
 // from the site defined by url
 function getJSON(url) {
-    var resp;
-    var xmlHttp;
+  var resp;
+  var xmlHttp;
 
-    resp = '';
-    xmlHttp = new XMLHttpRequest();
+  resp = '';
+  xmlHttp = new XMLHttpRequest();
 
-    if (xmlHttp !== null) {
-        xmlHttp.open("GET", url, false);
-        xmlHttp.send(null);
-        resp = xmlHttp.responseText;
-    }
+  if (xmlHttp !== null) {
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    resp = xmlHttp.responseText;
+  }
 
-    return resp;
+  return resp;
 }
 
 // onload event handler creates the URL
 // for a given year month and day
 function getBaseballData() {
-    var day = document.getElementById('day1').value;
-    var month = document.getElementById('month1').value;
-    var year = document.getElementById('year1').value;
+  var day = document.getElementById('day1').value;
+  var month = document.getElementById('month1').value;
+  var year = document.getElementById('year1').value;
 
-    var tempURL = "http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json";
+  var tempURL = "http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json";
 
-    var baseballJson = getJSON("http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json");
+  var baseballJson = getJSON("http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json");
 
-    baseballObject = JSON.parse(baseballJson);
+  baseballObject = JSON.parse(baseballJson);
 }
 
 //Gets the data from getBaseballData();
@@ -40,19 +40,21 @@ function getBaseballData() {
 //It will create the HTML and add the information to the div #toronto id
 //Then runs the createContent() function
 function getToronto(){
-    getBaseballData();
+  getBaseballData();
     // i element connects to font awesome
     var creDiv = "<div class='results'><i class='fa fa-arrow-right' aria-hidden='true'></i>";
     var totalDiv = "";
 
     for(var i = 0; i<baseballObject.data.games.game.length; i++){
-             var homeTeam = baseballObject.data.games.game[i].home_team_name;
-             var awayTeam = baseballObject.data.games.game[i].away_team_name;
-                 if(homeTeam == 'Blue Jays' || awayTeam == 'Blue Jays'){
-                    totalDiv += creDiv + "The home team is: " + homeTeam + ". The away team is: " + awayTeam;
-                    document.getElementById("toronto").innerHTML = totalDiv;
-                    break;
-                 }//End if
+     var homeTeam = baseballObject.data.games.game[i].home_team_name;
+     var awayTeam = baseballObject.data.games.game[i].away_team_name;
+     var homeScore = baseballObject.data.games.game[i].linescore.r.home;
+     var awayScore = baseballObject.data.games.game[i].linescore.r.away;
+     if(homeTeam == 'Blue Jays' || awayTeam == 'Blue Jays'){
+      totalDiv += creDiv + "The home team is: " + homeTeam + ". The away team is: " + awayTeam + "<br>" + homeTeam + " with " + homeScore + " runs <br>" + awayTeam + " with " + awayScore + " runs";
+      document.getElementById("toronto").innerHTML = totalDiv;
+      break;
+       }//End if
      }//End  for
      createContent();
  }//End getData();
@@ -60,21 +62,23 @@ function getToronto(){
 
 //Similar to getToronto(); but places all other games to the
 //div #output
- function createContent() {
+function createContent() {
     // i ele=memt connects with font awesome
-     var creDiv = "<div class='results'><i class='fa fa-arrow-right' aria-hidden='true'></i>";
-     var totalDiv = "";
+    var creDiv = "<div class='results'><i class='fa fa-arrow-right' aria-hidden='true'></i>";
+    var totalDiv = "";
 
-     for (var i = 0; i<baseballObject.data.games.game.length; i++){
-        var homeTeam = baseballObject.data.games.game[i].home_team_name;
-        var awayTeam = baseballObject.data.games.game[i].away_team_name;
+    for (var i = 0; i<baseballObject.data.games.game.length; i++){
+      var homeTeam = baseballObject.data.games.game[i].home_team_name;
+      var homeScore = baseballObject.data.games.game[i].linescore.r.home;
+      var awayTeam = baseballObject.data.games.game[i].away_team_name;
+      var awayScore = baseballObject.data.games.game[i].linescore.r.away;
 
-        if(homeTeam != 'Blue Jays' && awayTeam != 'Blue Jays'){
+      if(homeTeam != 'Blue Jays' && awayTeam != 'Blue Jays'){
 
-         totalDiv += creDiv + "The home team is: " + baseballObject.data.games.game[i].home_team_name + ". The away team is " + baseballObject.data.games.game[i].away_team_name;
-         document.getElementById("output").innerHTML = totalDiv;
+        totalDiv += creDiv + "The home team is: " + homeTeam + ". The away team is: " + awayTeam + "<br>" + homeTeam + " with " + homeScore + " runs <br>" + awayTeam + " with " + awayScore + " runs";
+       document.getElementById("output").innerHTML = totalDiv;
      }
-     }
+   }
  }
 
 
