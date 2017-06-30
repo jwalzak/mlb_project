37be5dbf6ffffc63e6347e35264ad13f25212a50
Jason Walzak <walzak@gmail.com>
2017-06-29 23:51:28 -0400

    When an input has fewer or more digits than four in the date column, a warning shows. Styled the labels.

--- scripts/main.js
+++ scripts/main.js
@@ -34,6 +34,10 @@ function getBaseballData() {
     month = '0' + month;
   }
 
+  if(year.length !== 4) {
+    document.querySelector(".year").innerHTML += 'Please enter the year in four digits';
+  }
+
   var tempURL = "http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json";
 
   var baseballJson = getJSON("http://gd2.mlb.com/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json");
